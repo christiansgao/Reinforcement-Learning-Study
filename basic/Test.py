4e73c9db09b77482b784d304865fe44df9279378
Christian Gao <christiansgao@gmail.com>
2017-09-23 21:32:07 -0700

    added code

---
+++ basic/Test.py
@@ -0,0 +1,5 @@
+from __future__ import division
+import math
+test = (1)/25;
+test = math.log10((0+1)/25)
+print(test)
---
+++ gym-ai-examples/Cartpole/BasicCartpole.py
@@ -0,0 +1,35 @@
+from __future__ import division
+
+import gym
+import numpy as np
+import random
+import math
+from time import sleep
+
+## Initialize the "Cart-Pole" environment
+env = gym.make('CartPole-v1')
+
+def simulate():
+
+    num_streaks = 0
+
+    for episode in range(100):
+
+        # Reset the environment
+        obv = env.reset()
+
+        for t in range(100):
+            env.render()
+
+            action =  env.step(env.action_space.sample()) # take a random action
+
+            # Execute the action
+            obv, reward, done, _ = env.step(action)
+
+def trainQTable(obv, reward):
+    print "Training";
+
+
+
+if __name__ == "__main__":
+    simulate()
