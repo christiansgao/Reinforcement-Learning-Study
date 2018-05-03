ee0b6244d1351499c9bb90bda6f9829ea25f61ab
Christian Gao <christiansgao@gmail.com>
2018-05-03 01:09:46 -0700

    Started Optimizer

--- hashlearner/helper/mnist/NodeOptimizer.py
+++ hashlearner/helper/mnist/NodeOptimizer.py
@@ -17,6 +17,7 @@ from hashlearner.mnistmodel.MnistModel import MnistModel
 from hashlearner.mnistnodes.SimpleHBaseMnistNode import SimpleHBaseMnistNode
 import csv
 import itertools
+from numpy import array
 
 
 def csv_writer(data, path):
@@ -27,27 +28,23 @@ def csv_writer(data, path):
         writer = csv.writer(csv_file, delimiter=',')
         for line in data:
             writer.writerow(line)
-#----------------------------------------------------------------------
-if __name__ == "__main__":
-    data = ["first_name,last_name,city".split(","),
-            "Tyrese,Hirthe,Strackeport".split(","),
-            "Jules,Dicki,Lake Nickolasville".split(","),
-            "Dedric,Medhurst,Stiedemannberg".split(",")
-            ]
-    path = "output.csv"
-    csv_writer(data, path)
 
-def get_convolution_set(convolve_range: tuple):
-    first_convolution = itertools.combinations(range(0,10,2))
+def log():
     pass
 
-get_convolution_set((0,10))
+def get_convolution_set(convolve_range: tuple, convolution_density: int):
+    convolution_list = list(itertools.combinations(range(convolve_range[0], convolve_range[1],convolution_density),2))
+    return convolution_list
+
 
 def optimize_simple_nodes(mnist_data):
-    with open(path, "wb") as csv_file:
-        train, test = sk_model.train_test_split(mnist_data, test_size=0.2)
 
-
+    train, test = sk_model.train_test_split(mnist_data, test_size=0.2)
+    convolution_set =list(itertools.combinations(np.linspace(1,28,1),r=2))
+    down_scale_ratios = array(list(range(1,10,1)))/10
+    binarize_threshold = list(range(10,250,10))
+    parameter_list = list(itertools.product(convolution_set, binarize_threshold))
+    pass
 
 
 def get_prediction_rate(simple_mnist_node: SimpleHBaseMnistNode, train_mnist:list, test_mnist: list):
@@ -77,7 +74,7 @@ def main():
     mnist_data = MnistLoader.read_mnist()
     mnist_data = mnist_data[:100]
 
-    optimize_simple_nodes()
+    optimize_simple_nodes(mnist_data)
 
     t1 = time.time()
     print("Total Time taken: " + str(t1 - t0) + " Seconds")
