7ca93c49ded3f1962c061db0f289100f30e8e0e5
christian <christiansgao@gmail.com>
2018-05-14 13:43:26 -0700

    added data

--- hashlearner/optimizers/NodeRunner.py
+++ hashlearner/optimizers/NodeRunner.py
@@ -1,52 +1,57 @@
-import itertools
-import random
-import re
+
 import time
-from multiprocessing import Pool
-from multiprocessing.pool import ThreadPool
 
 import numpy as np
 import sklearn.metrics as sk_metrics
 import sklearn.model_selection as sk_model
-from happybase import ConnectionPool
 
-from hashlearner.mnistnodes.MnistNode import MnistNode
+import hashlearner.helper.CSVHelper as CSVHelper
+from hashlearner.helper.mnist import MnistLoader
 from hashlearner.mnistnodes.SimpleHBaseMnistNode import SimpleHBaseMnistNode
-from hashlearner.helper.mnist import MnistLoader, MnistHelper
-from hbase.HBaseManager import HBaseManager, HBaseRow
-from hashlearner.mnistmodel.MnistModel import MnistModel
+from string import Template
+from hashlearner.mnistnodes.MnistNode import MnistNode
+
 
 def execute_iterations():
 
-    nodes = []
+    mnist_data = MnistLoader.read_mnist()
+    train = mnist_data[:50000]
+    test = mnist_data[50000:]
+    nodes = [SimpleHBaseMnistNode(convolve_shape=(8, 13), binarize_threshold=200, down_scale_ratio=.4),
+             SimpleHBaseMnistNode(convolve_shape=(5, 9), binarize_threshold=160, down_scale_ratio=.6),
+             SimpleHBaseMnistNode(convolve_shape=(4, 6), binarize_threshold=200, down_scale_ratio=.8),
+             SimpleHBaseMnistNode(convolve_shape=(12, 6), binarize_threshold=150, down_scale_ratio=.5)
+             ]
 
     for node in nodes:
-        run_node()
+        run_node(node, train, test)
 
-def run_node(node: SimpleHBaseMnistNode):
-    print("starting hbase mnist node")
-    mnist_data = MnistLoader.read_mnist()
-    mnist_data = mnist_data[:100]
+def extract_name(node: SimpleHBaseMnistNode):
+    name_template = Template("${name}_${conv_1}.${conv_2}_${thresh}_${scale}")
+    name = name_template.substitute(name= node.table_name, conv_1 = str(node.convolve_shape[0]), conv_2=str(node.convolve_shape[1]), thresh=node.binarize_threshold, scale=node.down_scale_ratio)
+    return name
 
+def run_node(mnist_node: SimpleHBaseMnistNode, train, test):
+    name = extract_name(mnist_node)
+    print("starting hbase mnist node" + name)
     t0 = time.time()
 
-    train, test = sk_model.train_test_split(mnist_data, test_size=0.1)
-
-    mnist_node = SimpleHBaseMnistNode(setup_table=True)
     status = mnist_node.train_node(train)
     print("training status: " + str(status))
 
-    true_numbers = [image[0] for image in test]
+    expected = [image[0] for image in test]
     test_images = [image[1] for image in test]
     print("Starting predictions")
 
     predictions = mnist_node.predict_from_images(test_images)
-    confusion_matrix = sk_metrics.confusion_matrix(y_true=true_numbers, y_pred=predictions)
+    confusion_matrix = sk_metrics.confusion_matrix(y_true=expected, y_pred=predictions)
 
     correct_classifications = np.diagonal(confusion_matrix);
     success_rate = sum(correct_classifications) / np.sum(confusion_matrix)
 
-    print("true numbers: " + str(true_numbers))
+    CSVHelper.write_predictions(expected, predictions, name=name)
+
+    print("true numbers: " + str(expected))
     print("predictions: " + str(predictions))
 
     print("Average Success Rate is: " + str(success_rate))
